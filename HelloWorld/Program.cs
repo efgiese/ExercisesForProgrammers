b365c36d7ea4131cc205fd0fe4bea5fdf55199c4
Edgar Giese <edgar@egiese.de>
2018-11-25 14:50:34 +0100

    Third step

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,10 +4,20 @@ namespace HelloWorld
 {
     class Program
     {
+        static string ReadInput(string prompt)
+        {
+            string result = "";
+            do
+            {
+                Console.WriteLine(prompt);
+                result = Console.ReadLine();
+            } while (result == "");
+
+            return result;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            Console.WriteLine("Hello, {0}, nice to meet you!", Console.ReadLine());
+            Console.WriteLine("Hello, {0}, nice to meet you!", ReadInput("What is your name?"));
         }
     }
 }
