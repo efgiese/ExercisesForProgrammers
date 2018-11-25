4e5137e6c327d1d5b410fdb6b8abae911b425180
Edgar Giese <edgar@egiese.de>
2018-11-25 18:05:52 +0100

    Step 2

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -4,10 +4,20 @@ namespace CountingCharacters
 {
     class Program
     {
+        static string ReadInput(string prompt)
+        {
+            string result = "";
+            do
+            {
+                Console.WriteLine(prompt);
+                result = Console.ReadLine();
+            } while (result == "");
+
+            return result;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("What is the input string?");
-            string input = Console.ReadLine();
+            string input = ReadInput("What is the input string?");
             Console.WriteLine("{0} has {1} characters.", input, input.Length);
         }
     }
