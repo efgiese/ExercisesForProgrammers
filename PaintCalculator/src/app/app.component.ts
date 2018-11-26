c21f9ede4d130f4c3ec89f38b94e26cbbb5aad01
Edgar Giese <edgar@egiese.de>
2018-11-26 20:59:42 +0100

    Setup Project

--- PaintCalculator/src/app/app.component.ts
+++ PaintCalculator/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PaintCalculator';
+  title = 'Paint Calculator';
+  aLenght: number;
+  aWidth: number;
+
+  onSubmit() {}
 }
