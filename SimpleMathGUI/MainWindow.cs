97246271a19d5213bc3114671fa7ff82f30c8487
Edgar Giese <edgar@egiese.de>
2018-11-26 17:34:17 +0100

    Implement GUI

---
+++ SimpleMathGUI/MainWindow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+	int first = 0;
+	int second = 1;
+
+	static int ReadInt(string input, int low, int high)
+	{
+		int result = 1;
+		bool success = Int32.TryParse (input, out result);
+		if (!success)
+		{
+			result = 1;
+		}
+		return (result >= low) && (result <= high) ? result : 1;
+	}
+
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnTextview3KeyReleaseEvent (object o, KeyReleaseEventArgs args)
+	{
+		first = ReadInt (textview3.Buffer.Text, 1, 100);
+		UpdateLabel ();
+	}
+
+	protected void OnTextview4KeyReleaseEvent (object o, KeyReleaseEventArgs args)
+	{
+		second = ReadInt (textview4.Buffer.Text, 1, 100);
+		UpdateLabel ();
+	}
+
+	private void UpdateLabel()
+	{
+		label3.Text = String.Format ($"{first} + {second} = {first + second}\n{first} - {second} = {first - second}\n{first} * {second} = {first * second}\n{first} / {second} = {first / second}\n");
+	}
+}
