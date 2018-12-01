ea634c1ed78014f6ca94ee70d19621627357d03a
Edgar Giese <edgar@egiese.de>
2018-12-01 17:54:59 +0100

    Setup project

---
+++ SelfCheckout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelfCheckout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
